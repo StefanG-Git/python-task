e3b36e63aa7002b4991885e1c3f429eba8b49492
Stefan Geranliev <s.geranliev@abv.bg>
2022-11-20 18:52:00 +0200

    Added data_processing_job.py

---
+++ python-task/data_processing_job.py
@@ -0,0 +1,15 @@
+class DataProcessingJob:
+    def __init__(self):
+        pass
+
+    def run(self):
+        pass
+
+    def _extract(self):
+        pass
+
+    def _transform(self):
+        pass
+
+    def _load(self):
+        pass
