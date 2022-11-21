f4d09c649926524c7788a8fe41b3599f666e4160
Stefan Geranliev <s.geranliev@abv.bg>
2022-11-21 21:48:49 +0200

    Added new logic in to _transform method

--- python-task/data_processing_job.py
+++ python-task/data_processing_job.py
@@ -2,11 +2,14 @@ from typing import List
 
 import pandas as pd
 
-from utils.data_transformations import *
-from utils.requests import *
+from utils.pandas_utils import *
+from utils.request_utils import *
 
 OUTER_MERGE = "outer"
+SUFFIXES = ("", "_drop")
+
 KURZNAME_COLUMN = "kurzname"
+HU_COLUMN = "hu"
 
 
 class DataProcessingJob:
@@ -34,7 +37,8 @@ class DataProcessingJob:
 
     def run(self):
         data = self._extract()
-        self._transform(data)
+        final_data = self._transform(data)
+        self._load(final_data)
 
     def _extract(self):
         # Read local data into DataFrame
@@ -48,9 +52,20 @@ class DataProcessingJob:
 
     def _transform(self, data):
         local_data_df, request_data_df = data
+        # Get the common columns from both DataFrames
+        common_columns = get_common_columns(request_data_df, local_data_df)
+        # Remove the merge column
+        common_columns.remove(KURZNAME_COLUMN)
         # Merge both DataFrames
-        merged_df = merge_dataframes(local_data_df, request_data_df, OUTER_MERGE, KURZNAME_COLUMN)
+        merged_df = merge_dataframes(local_data_df, request_data_df, OUTER_MERGE, KURZNAME_COLUMN, SUFFIXES)
+        # Filter rows where "hu" column is None
+        filtered_df = filter_rows_with_none_values(merged_df, HU_COLUMN)
+        # Replace None values from the duplicate column
+        clean_df = replace_none_values(filtered_df, common_columns)
+        # Drop the duplicate columns
+        clean_df = drop_duplicate_columns(clean_df, common_columns)
 
+        clean_df.to_excel('C:/Users/Stefan/Desktop/clean_file.xlsx')
 
-    def _load(self):
+    def _load(self, final_data):
         pass
