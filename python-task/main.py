bf51d3b63b3b003c3f405f864449caed13cebcc1
Stefan Geranliev <s.geranliev@abv.bg>
2022-11-21 12:23:58 +0200

    Added ArgumentParser

--- python-task/main.py
+++ python-task/main.py
@@ -1,5 +1,14 @@
+import argparse
+from distutils import util
+
 from data_processing_job import DataProcessingJob
 
+parser = argparse.ArgumentParser(description="Enter columns and whether to color rows")
+
+parser.add_argument("-k", "--keys", type=str, nargs="+", required=True)
+parser.add_argument("-c", "--colored", type=lambda x: bool(util.strtobool(x)), default=True)
+args = parser.parse_args()
+
 if __name__ == "__main__":
-    job = DataProcessingJob()
+    job = DataProcessingJob(args.keys, args.colored)
     job.run()
