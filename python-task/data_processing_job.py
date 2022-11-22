2a0026428d04a8a0c21b110e28cabceead930298
Stefan Geranliev <s.geranliev@abv.bg>
2022-11-22 14:33:19 +0200

    Added more logic to _transform method

--- python-task/data_processing_job.py
+++ python-task/data_processing_job.py
@@ -2,14 +2,15 @@ from typing import List
 
 import pandas as pd
 
-from utils.pandas_utils import *
+from utils.data_utils import *
 from utils.request_utils import *
 
 OUTER_MERGE = "outer"
 SUFFIXES = ("", "_drop")
 
-KURZNAME_COLUMN = "kurzname"
 HU_COLUMN = "hu"
+GRUPPE_COLUMN = "gruppe"
+KURZNAME_COLUMN = "kurzname"
 
 
 class DataProcessingJob:
@@ -31,6 +32,8 @@ class DataProcessingJob:
         "Content-Type": "application/json"
     }
 
+    _COLOR_REQUEST_URL = "https://api.baubuddy.de/dev/index.php/v1/labels/"
+
     def __init__(self, columns: List[str], to_color_rows: bool):
         self.columns = columns
         self.to_color_rows = to_color_rows
@@ -44,14 +47,15 @@ class DataProcessingJob:
         # Read local data into DataFrame
         local_data_df = pd.read_csv(self._LOCAL_DATA_PATH, sep=";")
         # Download resource data
-        data = get_request_resource(self._RESOURCE_REQUEST_URL, self._RESOURCE_REQUEST_HEADERS)
-        # Create DataFrame from the request data
-        request_data_df = pd.DataFrame(data)
+        resource_data = get_request_resource(self._RESOURCE_REQUEST_URL, self._RESOURCE_REQUEST_HEADERS)
+        # Create DataFrame from the resource data
+        request_data_df = pd.DataFrame(resource_data)
 
         return local_data_df, request_data_df
 
     def _transform(self, data):
         local_data_df, request_data_df = data
+
         # Get the common columns from both DataFrames
         common_columns = get_common_columns(request_data_df, local_data_df)
         # Remove the merge column from the common columns list
@@ -64,7 +68,15 @@ class DataProcessingJob:
         clean_df = replace_none_values(filtered_df, common_columns)
         # Drop the duplicate columns
         clean_df = drop_duplicate_columns(clean_df, common_columns)
+        # Sort DataFrame by "gruppe" column
+        sorted_df = sort_dataframe(clean_df, GRUPPE_COLUMN, True)
 
+        # for rnr in clean_df["rnr"]:
+        #     URL_2 = f'https://api.baubuddy.de/dev/index.php/v1/labels/{rnr}'
+        #     labelid_data_as_json = get_request_resource(URL_2, self._RESOURCE_REQUEST_HEADERS)
+        #     print(labelid_data_as_json)
+
+        sorted_df.to_excel('C:/Users/Stefan/Desktop/sorted_file.xlsx')
 
     def _load(self, final_data):
         pass
