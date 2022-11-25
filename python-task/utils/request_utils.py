166c2afe445f7a1a1dbde4cf318d1a78c3ee7332
Stefan Geranliev <s.geranliev@abv.bg>
2022-11-25 18:26:07 +0200

    Edited type hints

--- python-task/utils/request_utils.py
+++ python-task/utils/request_utils.py
@@ -1,15 +1,15 @@
-from typing import List
+from typing import List, Dict
 
 import requests
 
 
-def get_access_token(url: str, json: dict, headers: dict) -> dict:
+def get_access_token(url: str, json: dict, headers: Dict[str, str]) -> dict:
     response = requests.request("POST", url=url, json=json, headers=headers)
 
     return response.json()["oauth"]["access_token"]
 
 
-def get_request_resource(url: str, headers: dict, json: dict = None) -> List[dict]:
+def get_request_resource(url: str, headers: Dict[str, str], json: Dict[str, str] = None) -> List[dict]:
     response = requests.request("GET", url=url, json=json, headers=headers)
 
     return response.json()
