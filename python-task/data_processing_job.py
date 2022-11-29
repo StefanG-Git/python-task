86055332f07caa6d66e964bf001d64b32f8942d2
Stefan Geranliev <s.geranliev@abv.bg>
2022-11-29 18:58:00 +0200

    Added new functionality in the _transform method and implement the _load method

--- python-task/data_processing_job.py
+++ python-task/data_processing_job.py
@@ -1,12 +1,15 @@
+from datetime import datetime
 from typing import List
 
 import pandas as pd
+import openpyxl
 
 from utils.data_utils import *
-from utils.request_utils import *
+from utils.request_utils import get_access_token
 
 OUTER_MERGE = "outer"
 SUFFIX = "_drop"
+TODAY = datetime.now()
 
 
 class DataProcessingJob:
@@ -18,6 +21,8 @@ class DataProcessingJob:
     _COLOR_CODE_COLUMN = "colorCode"
 
     _LOCAL_DATA_PATH = "resources/vehicles.csv"
+
+    _OUTPUT_DATA_PATH = f"output_data/vehicles_{TODAY.isoformat()}.xlsx".replace(":", ".")
 
     _TOKEN_REQUEST_ULR = "https://api.baubuddy.de/index.php/login"
     _TOKEN_REQUEST_PAYLOAD = payload = {
@@ -40,19 +45,19 @@ class DataProcessingJob:
     _COLOR_REQUEST_URL = "https://api.baubuddy.de/dev/index.php/v1/labels/"
 
     def __init__(self, columns: List[str], to_color_rows: bool):
-        self.columns = columns + [self._RNR_COLUMN]
+        self.columns = [self._RNR_COLUMN, self._GRUPPE_COLUMN] + columns
         self.to_color_rows = to_color_rows
 
     def run(self):
         data = self._extract()
-        final_data = self._transform(data)
-        self._load(final_data)
+        result_wb = self._transform(data)
+        self._load(result_wb)
 
     def _extract(self):
         # Read local data into DataFrame
         local_data_df = pd.read_csv(self._LOCAL_DATA_PATH, sep=";")
         # Download resource data
-        resource_data = get_request_resource(self._RESOURCE_REQUEST_URL, self._RESOURCE_REQUEST_HEADERS)
+        resource_data = get_request_resource_as_json(self._RESOURCE_REQUEST_URL, self._RESOURCE_REQUEST_HEADERS)
         # Create DataFrame from the resource data
         request_data_df = pd.DataFrame(resource_data)
 
@@ -67,7 +72,7 @@ class DataProcessingJob:
         common_columns.remove(self._KURZNAME_COLUMN)
         # Merge both DataFrames
         merged_df = merge_dataframes(local_data_df, request_data_df, OUTER_MERGE, self._KURZNAME_COLUMN, ("", SUFFIX))
-        # Filter rows where "hu" column is None
+        # Filter rows where "hu" column is Null
         filtered_df = filter_rows_with_none_values(merged_df, self._HU_COLUMN)
         # Replace Null values from the duplicate column
         clean_df = replace_null_values(filtered_df, common_columns)
@@ -79,13 +84,28 @@ class DataProcessingJob:
         # Drop columns from the DataFrame that are not in the input
         clean_df = drop_mismatch_columns(sorted_df, self.columns)
 
+        # Create workbook
+        wb = openpyxl.Workbook()
+        ws = wb.active
+        # Write the data from pandas Dataframe to openpyxl workbook
+        write_data_from_pandas_dataframe_to_worksheet(clean_df, ws)
+
+        # Tint the cell's text in workbook if "labelIds" is given in the input
         if self._LABEL_IDS_COLUMN in self.columns:
-            clean_df = add_new_column(clean_df, self._COLOR_CODE_COLUMN)
-            clean_df = None
+            add_font_color_to_worksheet_cells(
+                clean_df[self._LABEL_IDS_COLUMN],
+                ws,
+                self._COLOR_REQUEST_URL,
+                self._RESOURCE_REQUEST_HEADERS
+            )
 
+        # Color rows by date in workbook if to_color_rows is True
         if self.to_color_rows:
-            pass
+            add_background_color_to_worksheet_cells(ws, self._HU_COLUMN, TODAY)
 
+        return wb
 
-    def _load(self, final_data):
-        pass
+    def _load(self, result_wb):
+        # Save the data after the transformations as xlsx file
+        result_wb.save(self._OUTPUT_DATA_PATH)
+        result_wb.close()
