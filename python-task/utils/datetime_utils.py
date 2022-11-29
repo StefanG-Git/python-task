28eb052828a95423e86e5531f0a9dc033e748272
Stefan Geranliev <s.geranliev@abv.bg>
2022-11-29 17:23:01 +0200

    Added datetime_utils.py file with string_to_date and get_months_diff_between_dates functions

---
+++ python-task/utils/datetime_utils.py
@@ -0,0 +1,42 @@
+from datetime import datetime
+
+from dateutil.relativedelta import relativedelta
+
+
+def string_to_date(date_str: str) -> datetime.date:
+    """
+    Convert string to date object.
+
+    Parameters
+    ----------
+    date_str : str
+        The date as string.
+
+    Returns
+    -------
+    date_obj : datetime.date
+        Date object.
+    """
+    date_obj = datetime.strptime(date_str, '%Y-%m-%d').date()
+    return date_obj
+
+
+def get_months_diff_between_dates(start_date: datetime.date, end_date: datetime.date):
+    """
+    Calculate months between two dates.
+
+    Parameters
+    ----------
+    start_date : datetime.date
+        Start date.
+    end_date: datetime.date
+        End date.
+
+    Returns
+    -------
+    months_diff : int
+        Months between the two dates.
+    """
+    delta = relativedelta(end_date, start_date)
+    months_diff = 12 * delta.years + delta.months
+    return months_diff
