7680e683c993771075f3547dc25b6b134a269ae0
Stefan Geranliev <s.geranliev@abv.bg>
2022-11-29 18:56:56 +0200

    Added new functions and edited doc strings

--- python-task/utils/data_utils.py
+++ python-task/utils/data_utils.py
@@ -1,14 +1,24 @@
 from typing import List, Dict, Any
 
+import openpyxl
 import pandas as pd
+from openpyxl.styles import PatternFill
+from openpyxl.utils.dataframe import dataframe_to_rows
 
-from utils.request_utils import get_request_resource
+from utils.common_utils import get_color_code_by_number
+from utils.datetime_utils import *
+from utils.request_utils import get_request_resource_as_json
 
 
-def merge_dataframes(left_df: pd.DataFrame, right_df: pd.DataFrame,
-                     merge_type: str, merge_columns: str | List[str], suffixes: tuple) -> pd.DataFrame:
+def merge_dataframes(
+        left_df: pd.DataFrame,
+        right_df: pd.DataFrame,
+        merge_type: str,
+        merge_columns: str | List[str],
+        suffixes: tuple
+) -> pd.DataFrame:
     """
-    Merge two dataframes by given columns.
+    Merges two dataframes by given columns.
 
     Parameters
     ----------
@@ -35,7 +45,7 @@ def merge_dataframes(left_df: pd.DataFrame, right_df: pd.DataFrame,
 
 def filter_rows_with_none_values(df: pd.DataFrame, column: str) -> pd.DataFrame:
     """
-    Remove rows from DataFrame where value from given column is Null.
+    Removes rows from DataFrame where value from given column is Null.
 
     Parameters
     ----------
@@ -56,7 +66,7 @@ def filter_rows_with_none_values(df: pd.DataFrame, column: str) -> pd.DataFrame:
 
 def get_common_columns(first_df: pd.DataFrame, second_df: pd.DataFrame) -> list:
     """
-    Find the common column names from two DataFrames.
+    Finds the common column names from two DataFrames.
 
     Parameters
     ----------
@@ -77,7 +87,7 @@ def get_common_columns(first_df: pd.DataFrame, second_df: pd.DataFrame) -> list:
 
 def replace_null_values(df: pd.DataFrame, common_columns: List[str]) -> pd.DataFrame:
     """
-    Compare the generated columns from DataFrame after merge
+    Compares the generated columns from DataFrame after merge
     and check if one is Null, then takes the value from the other.
 
     Parameters
@@ -100,7 +110,7 @@ def replace_null_values(df: pd.DataFrame, common_columns: List[str]) -> pd.DataF
 
 def drop_suffix_columns(df: pd.DataFrame, columns: List[str], suffix: str) -> pd.DataFrame:
     """
-    Drop redundant columns from DataFrame generated after merge.
+    Drops redundant columns from DataFrame generated after merge.
 
     Parameters
     ----------
@@ -123,7 +133,7 @@ def drop_suffix_columns(df: pd.DataFrame, columns: List[str], suffix: str) -> pd
 
 def drop_mismatch_columns(df: pd.DataFrame, columns: List[str]):
     """
-    Compare and drop columns from DataFrame
+    Compares and drop columns from DataFrame
     that are not in the given list.
 
     Parameters
@@ -145,7 +155,7 @@ def drop_mismatch_columns(df: pd.DataFrame, columns: List[str]):
 
 def sort_dataframe(df: pd.DataFrame, sort_columns: str | List[str], ascending: bool | List[bool]) -> pd.DataFrame:
     """
-    Sort DataFrame by given columns in given order.
+    Sorts DataFrame by given columns in given order.
 
     Parameters
     ----------
@@ -166,24 +176,190 @@ def sort_dataframe(df: pd.DataFrame, sort_columns: str | List[str], ascending: b
     return new_df
 
 
-def add_new_column(df: pd.DataFrame, column_name: str, value: Any = None):
+def add_new_column_to_dataframe(df: pd.DataFrame, column_name: str, value: Any = None):
     """
-    Add new column in DataFrame.
+    Adds new column in DataFrame with given value.
 
     Parameters
     ----------
-    df : pd.DataFrame
-        DataFrame use for adding the new column.
-    column_name : str
+    df: pd.DataFrame
+        DataFrame to use for adding the new column.
+    column_name: str
         Name of the new column.
     value: Any
-        Value to add in the new column.
+        Value to insert in the new column, None by default.
 
     Returns
     -------
-    df : pd.DataFrame
+    df: pd.DataFrame
         Dataframe with the new column.
     """
     df[column_name] = value
 
     return df
+
+
+def write_data_from_pandas_dataframe_to_worksheet(
+        df: pd.DataFrame,
+        ws: openpyxl.worksheet.worksheet.Worksheet
+) -> None:
+    """
+    Writes data from pandas Dataframe to openpyxl Worksheet.
+
+    Parameters
+    ----------
+    df: pd.DataFrame
+        DataFrame to use for getting the data.
+    ws: openpyxl.worksheet.worksheet.Worksheet
+        Worksheet to use for writing the data.
+
+    Returns
+    -------
+    None
+    """
+    for row in dataframe_to_rows(df, index=False, header=True):
+        ws.append(row)
+
+    for cell in ws[1]:
+        cell.style = 'Pandas'
+
+
+def get_color_codes(
+        label_ids: pd.Series,
+        url: str,
+        headers=Dict[str, str]
+) -> List[str]:
+    """
+    Extracts color codes from API via request using values from array.
+
+    Parameters
+    ----------
+    label_ids: pd.Series
+        Array containing the values.
+    url: str
+        URL of the color codes.
+    headers: Dict[str, str]
+        Headers to send with the request.
+
+    Returns
+    -------
+    color_codes: List[str]
+        List with all the extracted color codes
+    """
+    color_codes = []
+
+    for label_id in label_ids:
+        if label_id is not None:
+            current_url = f"{url}{label_id}"
+            resource = get_request_resource_as_json(current_url, headers)
+            try:
+                current_code = resource[0]["colorCode"]
+                color_codes.append(current_code)
+            except (IndexError, KeyError):
+                pass
+
+    return color_codes
+
+
+def add_font_color_to_worksheet_cells(
+        label_ids: pd.Series,
+        ws: openpyxl.worksheet.worksheet.Worksheet,
+        url: str,
+        headers: Dict[str, str]
+) -> None:
+    """
+    Adds font color to Worksheet's cells text by
+    extracting color code from API using values from array.
+
+    Parameters
+    ----------
+    label_ids: pd.Series
+        Array containing the values.
+    ws: openpyxl.worksheet.worksheet.Worksheet
+        Worksheet with the data.
+    url: str
+        URL of the color codes.
+    headers: Dict[str, str]
+        Headers to send with the request.
+
+    Returns
+    -------
+    None
+    """
+    color_codes = get_color_codes(label_ids, url, headers)
+    if not color_codes == []:
+        first_code = color_codes[0].lstrip("#")
+        rows_range = ws.max_row
+        columns_range = ws.max_column
+        for row_index in range(1, rows_range + 1):
+            for column_index in range(1, columns_range + 1):
+                ws.cell(row_index, column_index).font = openpyxl.styles.Font(color=first_code)
+
+
+def get_column_index_in_worksheet(
+        ws: openpyxl.worksheet.worksheet.Worksheet,
+        required_column,
+        rows_range,
+        columns_range
+) -> int:
+    """
+    Finds the column index of column in Worksheet.
+
+    Parameters
+    ----------
+    ws: openpyxl.worksheet.worksheet.Worksheet
+        Worksheet with the data.
+    required_column: str
+        Name of the required column
+    rows_range:
+        Worksheet's rows count
+    columns_range:
+        Worksheet's columns count
+
+    Returns
+    -------
+    column_index: int
+        The index of the required column
+    """
+    for row_index in range(1, rows_range + 1):
+        for column_index in range(1, columns_range + 1):
+            if ws.cell(row_index, column_index).value == required_column:
+                return column_index
+
+
+def add_background_color_to_worksheet_cells(
+        ws: openpyxl.worksheet.worksheet.Worksheet,
+        required_column: str,
+        today: datetime
+) -> None:
+    """
+    Adds background color to Worksheet's cells
+    depending on months count between two dates.
+
+    Parameters
+    ----------
+    ws: openpyxl.worksheet.worksheet.Worksheet
+        Worksheet with the data.
+    required_column: str
+        Column to use for start date
+    today: datetime
+        End date
+
+    Returns
+    -------
+    None
+    """
+    rows_range = ws.max_row
+    columns_range = ws.max_column
+
+    hu_column_index = get_column_index_in_worksheet(ws, required_column, rows_range, columns_range)
+
+    for row_index in range(1, rows_range + 1):
+        if hu_column_index is not None and row_index > 1:
+            date_str = ws.cell(row_index, hu_column_index).value
+            date = string_to_date(date_str)
+            months_diff = get_months_diff_between_dates(date, today)
+            color_code = get_color_code_by_number(months_diff)
+            for column_index in range(1, columns_range + 1):
+                cell_header = ws.cell(row_index, column_index)
+                cell_header.fill = PatternFill(start_color=color_code, fill_type="solid")
