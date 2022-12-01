b59d5f542ff2a7ec9b5c3e73a7d16b9aaf972b2e
Stefan Geranliev <s.geranliev@abv.bg>
2022-12-01 17:10:04 +0200

    Edited description in ArgumentParser

--- python-task/main.py
+++ python-task/main.py
@@ -3,7 +3,7 @@ from distutils import util
 
 from data_processing_job import DataProcessingJob
 
-parser = argparse.ArgumentParser(description="Enter columns and whether to color rows")
+parser = argparse.ArgumentParser(description="Enter columns to include and whether to add background color on rows")
 
 parser.add_argument("-k", "--keys", type=str, nargs="+", required=True)
 parser.add_argument("-c", "--colored", type=lambda x: bool(util.strtobool(x)), default=True)
