ac78c3b61c809892bc2fb83953d1c2d946f62d85
Stefan Geranliev <s.geranliev@abv.bg>
2022-12-01 20:29:49 +0200

    Added comments

--- python-task/tests/test_datetime_utils.py
+++ python-task/tests/test_datetime_utils.py
@@ -21,42 +21,57 @@ class DatetimeUtilsTests(TestCase):
     _DIFF_YEAR_END_DATE_AS_STR = "2022-01-12"
 
     def test_string_to_date__when_date_is_separated_with_dashes__expect_to_be_converted_to_date_object(self):
+        # Arrange
         expected_result = datetime.strptime(self._DASH_SEPARATED_DATE_AS_STR, self._DASH_SEPARATED_DATE_FORMAT).date()
 
+        # Act
         actual_result = string_to_date(self._DASH_SEPARATED_DATE_AS_STR)
 
+        # Assert
         self.assertEqual(expected_result, actual_result)
 
     def test_string_to_date__when_date_is_separated_with_slashes__expect_to_be_converted_to_date_object(self):
+        # Arrange
         expected_result = datetime.strptime(self._SLASH_SEPARATED_DATE_AS_STR, self._SLASH_SEPARATED_DATE_FORMAT).date()
 
+        # Act
         actual_result = string_to_date(self._SLASH_SEPARATED_DATE_AS_STR)
 
+        # Assert
         self.assertEqual(expected_result, actual_result)
 
     def test_get_months_diff_between_dates__when_dates_are_in_same_month__expect_0_months_diff(self):
+        # Arrange
         expected_result = 0
 
+        # Act
         start_date = string_to_date(self._SAME_MONTH_START_DATE_AS_STR)
         end_date = string_to_date(self._SAME_MONTH_END_DATE_AS_STR)
         actual_result = get_months_diff_between_dates(start_date, end_date)
 
+        # Assert
         self.assertEqual(expected_result, actual_result)
 
     def test_get_months_diff_between_dates__when_dates_are_in_diff_months__expect_correct_months_diff(self):
+        # Arrange
         expected_result = 4
 
+        # Act
         start_date = string_to_date(self._DIFF_MONTH_START_DATE_AS_STR)
         end_date = string_to_date(self._DIFF_MONTH_END_DATE_AS_STR)
         actual_result = get_months_diff_between_dates(start_date, end_date)
 
+        # Assert
         self.assertEqual(expected_result, actual_result)
 
     def test_get_months_diff_between_dates__when_period_between_dates_is_one_year__expect_12_months_diff(self):
+        # Arrange
         expected_result = 12
 
+        # Act
         start_date = string_to_date(self._DIFF_YEAR_START_DATE_AS_STR)
         end_date = string_to_date(self._DIFF_YEAR_END_DATE_AS_STR)
         actual_result = get_months_diff_between_dates(start_date, end_date)
 
+        # Assert
         self.assertEqual(expected_result, actual_result)
